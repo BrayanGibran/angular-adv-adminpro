f4b678c5e9c82c22137b493b173336c2db342fd1
Brayan Gibran <brayan.gibran@gmail.com>
2023-04-04 12:53:56 -0600

    Fin de sección 8

---
+++ src/app/pages/promesas/promesas.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { resolve } from 'chart.js/dist/helpers/helpers.options';
+
+@Component({
+  selector: 'app-promesas',
+  templateUrl: './promesas.component.html',
+  styles: [],
+})
+export class PromesasComponent implements OnInit {
+  ngOnInit(): void {
+    this.getusuarios().then((usuarios) => {
+      console.log(usuarios);
+    });
+  }
+
+  getusuarios() {
+    return new Promise((resolve) => {
+      fetch('https://reqres.in/api/users')
+        .then((resp) => resp.json())
+        .then((body) => resolve(body.data));
+    });
+  }
+}
--- src/app/services/sidebar.service.ts
+++ src/app/services/sidebar.service.ts
@@ -1,21 +1,22 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class SidebarService {
-
   menu: any[] = [
     {
       titulo: 'Dashboard',
       icono: 'mdi mdi-gauge',
       submenu: [
         { titulo: 'Main', url: '/' },
-        { titulo: 'ProgressBar', url: 'progress' },
         { titulo: 'Gráficas', url: 'grafica1' },
-      ]
-    }
-  ]
+        { titulo: 'rxjs', url: 'rxjs' },
+        { titulo: 'Promesas', url: 'promesas' },
+        { titulo: 'ProgressBar', url: 'progress' },
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
 }
